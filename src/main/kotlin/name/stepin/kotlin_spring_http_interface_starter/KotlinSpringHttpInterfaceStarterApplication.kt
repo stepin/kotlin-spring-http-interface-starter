f94be9fbb99ae7a8a4bac2c93d283427dc925037
Igor Stepin <igor@stepin.name>
2024-07-02 09:35:42 +0400

    App structure

---
+++ src/main/kotlin/name/stepin/kotlin_spring_http_interface_starter/KotlinSpringHttpInterfaceStarterApplication.kt
@@ -0,0 +1,11 @@
+package name.stepin.kotlin_spring_http_interface_starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringHttpInterfaceStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringHttpInterfaceStarterApplication>(*args)
+}
---
+++ src/test/kotlin/name/stepin/kotlin_spring_http_interface_starter/KotlinSpringHttpInterfaceStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package name.stepin.kotlin_spring_http_interface_starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringHttpInterfaceStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
